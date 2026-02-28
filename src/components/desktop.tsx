4eea7e19581476c3889de95ae199e24df1f9ab7c
gustavoGGV <gustavo.vieira.tds2023@gmail.com>
2026-02-28 14:07:56 -0300

    feat: add wallpaper

---
+++ src/components/desktop.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import "./desktop.css";
+class Desktop extends React.Component {
+  render() { 
+    return (
+      <div className="desktop">
+      </div>
+    );
+  }
+}
+ 
+export default Desktop;
